bf025901c58624ce6e1d0e8bbc9d5f94f6c5afb6
Tales <not_functional@a.com>
2021-07-10 12:03:53 -0300

    first commit

---
+++ src/main/java/br/com/demo/ia/DemoIaBackApplication.java
@@ -0,0 +1,13 @@
+package br.com.demo.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIaBackApplication.class, args);
+	}
+
+}
